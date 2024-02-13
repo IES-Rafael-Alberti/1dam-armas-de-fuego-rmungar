45ea6544a39abbbc60cf9439d72337282f54a4da
rmungar <mgraul@iesrafaelalberti.es>
2024-02-13 20:23:03 +0100

    TODO COMENTADO - PARTE 1

--- src/TipoRadio.kt
+++ src/TipoRadio.kt
@@ -1,7 +1,7 @@
 package src
 
 /**
- * La enum class TipoRadio contiene los diferentes tipos de radios de impacto de las armas
+ * La enum class TipoRadio contiene los diferentes tipos de radios de impacto de las armas.
  */
 enum class TipoRadio(desc:String) {
     REDUCIDO("REDUCIDO"),CORTO("CORTO"),INTERMEDIO("INTERMEDIO"),AMPLIO("AMPLIO"),ENORME("ENORME")
